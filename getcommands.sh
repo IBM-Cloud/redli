d55033e8b24511adacd17ffc9289f33f264120cd
Dj Walker-Morgan <codepope@gmail.com>
2018-07-26 10:43:52 +0100

    Now creates the go file for inclusion.

--- getcommands.sh
+++ getcommands.sh
@@ -1,2 +1,5 @@
+mv commands.go commands.go.old
 curl -LO https://github.com/antirez/redis-doc/raw/master/commands.json
-
+echo "package main\n\nvar redisCommandsJSON = \`" > commands.go
+cat commands.json >> commands.go
+echo "\`" >> commands.go
